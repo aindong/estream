80d8900a507996d6fe6dbe6744edc304c8fe1cff
Alleo Indong <freedom_war550@live.com.ph>
2015-01-17 00:53:51 +0800

    Updated user schema

--- app/database/migrations/2015_01_16_040541_create_users_table.php
+++ app/database/migrations/2015_01_16_040541_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration {
 			$table->string('email', 100);
 			$table->string('password', 150);
 			$table->string('activation_code', 150);
-			$table->integer('activated', 1)->default(0);
+			$table->integer('activated');
 			$table->integer('role_id');
 			$table->string('last_ip', 50);
 			$table->timestamp('last_login_at');
