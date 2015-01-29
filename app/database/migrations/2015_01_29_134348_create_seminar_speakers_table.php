d92bdb6eaa122531b1aee39e6aaa5fa5eb5cd3e9
Alleo Indong <freedom_war550@live.com.ph>
2015-01-29 21:52:46 +0800

    seminar and seminar speakers schema

--- app/database/migrations/2015_01_29_134348_create_seminar_speakers_table.php
+++ app/database/migrations/2015_01_29_134348_create_seminar_speakers_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateSeminarsTable extends Migration {
+class CreateSeminarSpeakersTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,14 +12,15 @@ class CreateSeminarsTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('seminars', function(Blueprint $table)
+		Schema::create('seminar_speakers', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('title');
-			$table->mediumText('description');
-			$table->string('price');
-			$table->timestamp('start_at');
-			$table->timestamp('end_at');
+			$table->integer('seminar_id');
+			$table->string('fname');
+			$table->string('lname');
+			$table->string('mname');
+			$table->string('image');
+			$table->mediumText('bio');s
 			$table->timestamps();
 		});
 	}
@@ -32,7 +33,7 @@ class CreateSeminarsTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('seminars');
+		Schema::drop('seminar_speakers');
 	}
 
 }
