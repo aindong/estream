146415dcc59824870a66f559b3a1e9950d8f8173
Alleo Indong <freedom_war550@live.com.ph>
2015-02-07 16:00:59 +0800

    Update paid and waiting

--- app/views/emails/welcome.blade.php
+++ app/views/emails/welcome.blade.php
@@ -1,6 +1,6 @@
 <div class="container" style="font-family:calibri; margin: 20px 30px;">
     <hr style="height: 3px; background: #42454A; border:none"/>
-    <h1> Congratulations thank you for registering an account with AIERS </h1>
+    <h1> Congratulations thank you for registering an account with AEIRS </h1>
     {{ $user['first_name'] . ' ' . $user['last_name'] }}
     <p>User ID: {{ $user['email'] }}</p>
     <p>
