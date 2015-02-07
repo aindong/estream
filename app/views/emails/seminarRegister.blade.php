ffbe8a401635ba63639fd74ed029dbd176cb0f99
Alleo Indong <freedom_war550@live.com.ph>
2015-02-07 12:24:04 +0800

    Seminar registration email

--- app/views/emails/seminarRegister.blade.php
+++ app/views/emails/seminarRegister.blade.php
@@ -1,9 +1,5 @@
 <div class="container" style="font-family:calibri; margin: 20px 30px;">
     <hr style="height: 3px; background: #42454A; border:none"/>
-    <h1> Congratulations thank you for registering an account with AIERS </h1>
-    {{ $user['first_name'] . ' ' . $user['last_name'] }}
-    <p>User ID: {{ $user['email'] }}</p>
-    <p>
-        Have a great day ahead. Always visit our site to view the latest seminars that we are offering.
-    </p>
+    <h1> Congratulations, thank you registering on a seminar </h1>
+    <p>Current status: Waiting for payment</p>
 </div>
