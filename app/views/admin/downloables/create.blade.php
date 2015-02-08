aceb92ede4951a95172aba93449a1b9944bcd6b7
Alleo Indong <freedom_war550@live.com.ph>
2015-02-09 01:28:03 +0800

    Update

---
+++ app/views/admin/downloables/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h2>Upload downloadables</h2>
+    {{ Form::open(['route' => 'admin.dpwnloadables.store', 'method' => 'post', 'enctype' => "multipart/form-data"]) }}
+
+        <div class="form-group">
+            {{ Form::label('location', 'Location *', ['class' => 'form-label']) }}
+            {{ Form::text('location', null, ['class' => 'form-control controls', 'placeholder' => 'Type a location here and press enter', 'id' => 'locationSearch', 'required' => 'required']) }}
+        </div>
+
+        <div class="form-group">
+            {{ Form::label('file', 'Image *', ['class' => 'form-label']) }}
+            {{ Form::file('file',null, ['class' => 'form-control', 'required' => 'required']) }}
+        </div>
+
+        <a href="/admin/medias" class="btn btn-danger">Back</a>
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+    {{ Form::close() }}
+@stop
+
+@section('page-script')
+
+@stop
--- app/views/admin/seminars/show.blade.php
+++ app/views/admin/seminars/show.blade.php
@@ -5,6 +5,30 @@
     <p style="color: #000; font-size: 12px;">{{ date('d M, Y', strtotime($seminar->start_at)) }} - {{ date('d M, Y', strtotime($seminar->end_at)) }}</p>
     <p style="color: #000; font-size: 15px;">{{ substr($seminar->description, 0, 250) }} ...</p>
 
+    <h1>Downloadables <a href="/admin/downloables/create">Add</a></h1>
+    <table class="table table-striped table-bordered" cellspacing="0" width="100%" id="example">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($downloadables as $downloadable)
+            <tr>
+                <td>{{{ $downloadable->name }}}</td>
+                <td><a href="/admin/downloadables/{{ $downloadable->id }}/download" class="btn btn-danger">Download</a>
+                    {{ Form::open(array('url' => 'admin/downloadables/' . $downloadable->id, 'class' => 'deleteItem')) }}
+                    {{ Form::hidden('_method', 'DELETE') }}
+                    {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
+                    {{ Form::close() }}
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
+    <h1>Requesting to attend</h1>
     <table class="table table-striped table-bordered" cellspacing="0" width="100%" id="example">
         <thead>
         <tr>
