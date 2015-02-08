d8b3e29196270504b86ca15d2c5d0dcd3a4369b4
Alleo Indong <freedom_war550@live.com.ph>
2015-02-09 03:15:49 +0800

    created downloadables

--- app/routes.php
+++ app/routes.php
@@ -43,7 +43,7 @@ Route::get('/webcast/client', ['as' => 'webcast.client', 'uses' => 'WebcastsCont
 Route::get('/admin/downloadables', ['as' => 'downloadables.index', 'uses' => 'DownloadablesController@index']);
 Route::get('/admin/downloadables/{id}', ['as' => 'downloadables.show', 'uses' => 'DownloadablesController@download']);
 Route::get('/admin/downloadables/create', ['as' => 'admin.downloadables.create', 'before' => 'auth|hasRole:admin', 'uses' => 'DownloadablesController@create']);
-Route::post('/admin/downloadables', ['as' => 'admin.downloadables.store', 'before' => 'auth|hasRole:admin', 'uses' => 'DownloadablesController@create']);
+Route::post('/admin/downloadables', ['as' => 'admin.downloadables.store', 'before' => 'auth|hasRole:admin', 'uses' => 'DownloadablesController@store']);
 Route::delete('/admin/downloadables', ['as' => 'admin.downloadables.delete', 'before' => 'auth|hasRole:admin', 'uses' => 'DownloadablesController@destroy']);
 
 /**
