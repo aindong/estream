350aa891238affa2b04177ca78c6f9f2ac46ab8f
Alleo Indong <freedom_war550@live.com.ph>
2015-02-09 02:59:28 +0800

    created downloadables

--- app/controllers/DownloablesController.php
+++ app/controllers/DownloablesController.php
@@ -19,8 +19,24 @@ class DownloadablesController extends BaseController
         return View::make('admin.downloadables.create');
     }
 
-    public function store()
+    public function store($id)
     {
+        if (!Input::hasFile('invitation')) {
+            return \Redirect::back();
+        }
+
+        $destination = public_path() . '/public/uploads/downloadables/';
+
+        $file = Input::file('file');
+        $filename = uniqid() . '-' . time() . '-' . $file->getClientOriginalName();
+        $file->move($destination, $filename);
+        $file->invitation = $filename;
+
+        $downloadable = new \Downloadable();
+        $downloadable->seminar_id = $id;
+        $downloadable->name = Input::get('name');
+        $downloadable->filename = $filename;
+        $downloadable->save();
 
         return View::make('admin.downloadables.create');
     }
