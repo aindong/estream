e294baa481544b26215927c1bb38f4346bf833f0
Nina Kristel Cheng <nkcheng@koodi.ph>
2015-02-09 13:10:43 +0800

    modal downloadables

--- app/views/front/seminars-single.blade.php
+++ app/views/front/seminars-single.blade.php
@@ -104,14 +104,31 @@
             <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h3 id="myModalLabel">Modal header</h3>
+                <h3 id="myModalLabel">Downloadables</h3>
               </div>
               <div class="modal-body">
-                <p>One fine body…</p>
+
+                <table class="table table-striped table-bordered" cellspacing="0" width="100%" id="example">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($downloadables as $downloadable)
+                        <tr>
+                            <td>{{{ $downloadable->name }}}</td>
+                            <td><a href="/downloadables/{{ $downloadable->id }}/download" class="btn btn-primary">Download</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+
               </div>
               <div class="modal-footer">
                 <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-                <button class="btn btn-primary">Save changes</button>
               </div>
             </div>
 
