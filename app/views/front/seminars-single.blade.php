f5494d0334a2f5d713e8124896446e51c450f28b
Nina Kristel Cheng <nkcheng@koodi.ph>
2015-02-09 13:08:25 +0800

    modal downloadables

--- app/views/front/seminars-single.blade.php
+++ app/views/front/seminars-single.blade.php
@@ -71,7 +71,7 @@
                                     ?>
 
                                     @if($seminarStatus == "paid")
-                                        <a href="#" class="btn-style">Download Assets</a>
+                                        <a href="#myModal" class="btn-style">Download Assets</a>
                                     @elseif($seminarStatus == 'waiting for payment')
                                         Waiting for your payment
                                     @else
@@ -99,6 +99,21 @@
                 <!--EVENT LOCATION MAP END-->
             </div>
             <div class="clearfix"></div>
+
+            <!-- Modal -->
+            <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h3 id="myModalLabel">Modal header</h3>
+              </div>
+              <div class="modal-body">
+                <p>One fine body…</p>
+              </div>
+              <div class="modal-footer">
+                <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                <button class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
 
         </div>
         <!--FOLLOW US SECTION START-->
